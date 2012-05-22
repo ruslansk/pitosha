881e3de7f0e8aafda0e81384091f87df75e26b09
rus <ruslansk@list.ru>
2012-05-22 15:52:41 +0400

    fix env rename deco

--- 24.cmd
+++ 24.cmd
@@ -1,6 +1,7 @@
 @echo off
-set PYTHONHOME=D:\Programs\Devel\Python24
-set PYTHONPATH=D:\Programs\Devel\Python24\Lib;D:\Programs\Devel\Python24\DLLs;D:\Programs\Devel\Python24\Lib\lib-tk
-set PATH=D:\Programs\Devel\Python24\;D:\Programs\Devel\Python24\Scripts\;%PATH%
-cd D:\Programs\Devel\Python27_tests\uncompyle2\
+rem set PYTHONHOME=D:\Programs\Devel\Python24
+set PYTHONHOME=D:\Programs\Devel\Python
+set PYTHONPATH=%PYTHONHOME%\Lib;%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib\lib-tk
+set PATH=%PYTHONHOME%\;%PYTHONHOME%\Scripts\;%PATH%
+rem cd D:\Programs\Devel\Python27_tests\uncompyle2\
 cmd
