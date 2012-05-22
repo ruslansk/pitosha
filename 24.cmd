ce9d202604dd98841eee7f70aad4c8000734a4a8
rus <ruslansk@list.ru>
2012-05-22 16:05:44 +0400

    fix Qt4 env for python-2.4 with PyQt

--- 24.cmd
+++ 24.cmd
@@ -2,6 +2,6 @@
 rem set PYTHONHOME=D:\Programs\Devel\Python24
 set PYTHONHOME=D:\Programs\Devel\Python
 set PYTHONPATH=%PYTHONHOME%\Lib;%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib\lib-tk
-set PATH=%PYTHONHOME%\;%PYTHONHOME%\Scripts\;%PATH%
+set PATH=C:\Qt\4.8.1\bin\;%PYTHONHOME%\;%PYTHONHOME%\Scripts\;%PATH%
 rem cd D:\Programs\Devel\Python27_tests\uncompyle2\
 cmd
