b0f5be3089480783d50223acd9ffa15f87219e38
ruslansk <ruslansk@list.ru>
2014-02-10 11:20:30 +0400

    django: copy templates to project directory

--- django/mysite3/mysite3/settings.py
+++ django/mysite3/mysite3/settings.py
@@ -1,4 +1,5 @@
 # Django settings for mysite3 project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -113,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
